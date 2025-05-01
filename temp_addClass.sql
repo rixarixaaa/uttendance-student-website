03a41d9848ac42f9e77cbe559259b7edfd5004d0
rixarixaaa <111485215+rixarixaaa@users.noreply.github.com>
2025-05-01 01:20:14 -0500

    T

--- temp_addClass.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `class` (`CourseNum`, `SectionNum`, `ClassSubject`, `ClassNum`, `ClassName`, `FK_ImageID`) VALUES
-	(123456, 123, 'Fic', 1863, 'Three Ways to Survive and Apocalypse', NULL);
-	
-INSERT INTO `teaches` (`FK_INetID`, `FK_CourseNum`) VALUES
-	('SXH210003', 123456);
